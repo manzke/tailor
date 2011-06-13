a65f53fa084c63ab01fcbc3b0e963f26c9a6adb3
Daniel Manzke <daniel.manzke@googlemail.com>
2011-06-14 00:26:07 +0200

    added pause and resume. removed jersey

--- src/main/java/com/saperion/html/tail/TailorWebSocketServlet.java
+++ src/main/java/com/saperion/html/tail/TailorWebSocketServlet.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
@@ -15,8 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.websocket.WebSocket;
 import org.eclipse.jetty.websocket.WebSocketServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TailorWebSocketServlet extends WebSocketServlet {
+	private static Logger LOGGER = LoggerFactory.getLogger(TailorWebSocketServlet.class);
 	private static final long serialVersionUID = -7289719281366784056L;
 	public static String newLine = System.getProperty("line.separator");
 	
@@ -31,46 +35,57 @@ public class TailorWebSocketServlet extends WebSocketServlet {
 
 	public WebSocket doWebSocketConnect(HttpServletRequest request,
 			String protocol) {
+		
 		String logfile = request.getParameter("logfile");
-		if(logfile != null && logfile.length() > 0){
-			return new TailorSocket(logfile);	
+		if(logfile == null || logfile.length() == 0){
+			logfile = "unknown.txt";	
 		}
-		return new TailorSocket();
+
+		boolean newOnly = "on".equalsIgnoreCase(request.getParameter("newOnly"));
+		
+		return new TailorSocket(logfile, newOnly);
 	}
 
 	class TailorSocket implements WebSocket.OnTextMessage {
 		private Connection _connection;
-		private String logFileName;
-		private RandomAccessFile log;
-		private long lastLength;
+		private String _logFileName;
+		private RandomAccessFile _log;
+		private boolean _newOnly;
+		private Runnable _job;
+		private ScheduledFuture<?> _scheduledJob;
 		
 		public TailorSocket() {
-			logFileName = "unknown.txt";
+			_logFileName = "unknown.txt";
 		}
 		
-		public TailorSocket(String logfile){
-			logFileName = logfile;
+		public TailorSocket(String logfile, boolean newOnly){
+			this._logFileName = logfile;
+			this._newOnly = newOnly;
 		}
 		
 		@Override
 		public void onClose(int closeCode, String message) {
-			if(log != null){
+			if(_log != null){
 				try {
-					log.close();
+					_log.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					LOGGER.info(e.getMessage(), e);
 				}				
 			}
 			_members.remove(this);
 		}
 		
-		public void sendMessage(String data) throws IOException {
-			_connection.sendMessage(data);
-		}
-		
 		@Override
 		public void onMessage(String data) {
-			System.out.println("Received: "+data);
+			if("Pause".equalsIgnoreCase(data)){
+				if(_scheduledJob != null) _scheduledJob.cancel(true);
+			}else if("Resume".equalsIgnoreCase(data)){
+				if(_scheduledJob != null) schedule();
+			}
+		}
+		
+		public void sendMessage(String data) throws IOException {
+			_connection.sendMessage(data);
 		}
 		
 		public boolean isOpen() {
@@ -82,39 +97,47 @@ public class TailorWebSocketServlet extends WebSocketServlet {
 			_members.add(this);
 			_connection = connection;
 			try {
-				File logFile = new File(logFileName);
+				File logFile = new File(_logFileName);
 				if(!logFile.exists()){
 					connection.sendMessage("Log File \""+logFile.getAbsolutePath()+"\" doesn't exist. Closing Connection.");
 					connection.disconnect();
 				}else{
-					log = new RandomAccessFile(logFile, "r");	
-					connection.sendMessage("Log File \""+logFile.getAbsolutePath()+"\" found. Starting Tailing.");
-
-					executor.scheduleAtFixedRate(new Runnable() {
+					_log = new RandomAccessFile(logFile, "r");	
+					connection.sendMessage("Log File \""+logFile.getAbsolutePath()+"\" found. Starting Tailing. New Stuff only? "+_newOnly);
+					if(_newOnly){
+						_log.seek(_log.length());
+					}
+					_job = new Runnable() {
+						private long _lastLength;
 						@Override
 						public void run() {
 							if(isOpen()){
 								try {
-									long actualLength = log.length();
-									if(actualLength < lastLength){
+									long actualLength = _log.length();
+									if(actualLength < _lastLength){
 										//Rolling Log Files
-										log.seek(0);
+										_log.seek(0);
 									}
 									String line = null;
-									while((line = log.readLine()) != null){
+									while((line = _log.readLine()) != null){
 										_connection.sendMessage(line);
 									}
-									lastLength = log.length();
+									_lastLength = _log.length();
 								} catch (IOException e) {
-									e.printStackTrace();
+									LOGGER.warn("Error while trying to send a Log Line to the Web Socket-Client.", e);
 								}	
 							}
 						}
-					}, 2, 2, TimeUnit.SECONDS);
+					};
+					schedule();
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
+				LOGGER.warn("Error while trying to load or send a Log Line to the Web Socket-Client.", e);
 			}
+		}
+		
+		private void schedule(){
+			_scheduledJob = executor.scheduleAtFixedRate(_job, 2, 2, TimeUnit.SECONDS);
 		}
 	}
 }
